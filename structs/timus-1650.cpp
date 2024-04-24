8cbb32ab97fd6fb6d01dc8bf3aa5e5c54ceb18c7
geeks <geekslovee@gmail.com>
2024-04-25 00:25:13 +0300

    timus 1650 (6 test WA)

--- structs/timus-1650.cpp
+++ structs/timus-1650.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <unordered_map>
-#include <set>
+#include <unordered_set>
 #include <map>
 #include <string>
 
 using namespace std;
 
 int main() {
-    unordered_map<string, pair<long long, string>> billionaires; // name -> (new worth, city)
-    unordered_map<string, long long> city_sums;                  // city -> city_sum
-    map<long long, set<string>> money_sums;                      // city_sum -> city
-    map<string, int> amounts;                                    // city_name -> amount of max days
+    unordered_map<string, pair<long long, string>> billionaires;           // name -> (net worth, curr city)
+    unordered_map<string, long long> city_sums;                            // city -> city_sum
+    map<long long, unordered_set<string>, greater<>> money_sums;           // city_sum -> city
+    map<string, int> amounts;                                              // city_name -> amount of max days
 
     int n; cin >> n;
     for (int i = 0; i < n; i++) {
@@ -19,12 +19,9 @@ int main() {
         cin >> name >> city >> net_worth;
 
         billionaires[name] = {net_worth, city};
-        if (city_sums.find(city) != city_sums.end())
-            city_sums[city] += net_worth;
-        else
-            city_sums[city] = net_worth;
+        city_sums[city] += net_worth;
     }
-
+    
     for (auto& it : city_sums)
         money_sums[it.second].insert(it.first);
 
@@ -35,34 +32,51 @@ int main() {
         int day; string name; string city;
         cin >> day >> name >> city;
 
-        for (auto& it : money_sums.begin()->second) { // update days with max amount
-            if (amounts.find(it) != amounts.end()) amounts[it] += (day - prev_day);
-            else amounts[it] = (day - prev_day);
+        if (money_sums.begin()->second.size() == 1) {
+            auto elem = money_sums.begin()->second.begin();
+            amounts[*elem] += (day - prev_day);                                                                                    
         }
 
         long long start_city_sum = city_sums[billionaires[name].second];
-        long long old_new_city_sum = start_city_sum - billionaires[name].first;
+        long long start_new_city_sum = start_city_sum - billionaires[name].first;
 
-        city_sums[billionaires[name].second] -= billionaires[name].first;        // move net worth to city
+        city_sums[billionaires[name].second] -= billionaires[name].first;       // move net worth to city
         city_sums[city] += billionaires[name].first;
 
-        for (auto& it : money_sums[start_city_sum])  // move money_sums to decremented value
-            money_sums[old_new_city_sum].insert(it);
-        money_sums[old_new_city_sum].erase(billionaires[name].second);
-        money_sums.erase(start_city_sum);
-    
+        money_sums[start_city_sum].erase(billionaires[name].second);    // move money_sums to decremented value
+        if (money_sums[start_city_sum].size() == 0) money_sums.erase(start_city_sum);
+        
         long long new_sum_value = city_sums[city];
         money_sums[new_sum_value].insert(city);
 
-        prev_day = day;
+        money_sums[start_new_city_sum].insert(billionaires[name].second);
+        billionaires[name].second = city;
+        prev_day = day;                 
     }
-
-    for (auto& it : money_sums.begin()->second) {     // add rest days
-        if (amounts.find(it) != amounts.end()) amounts[it] += (days - prev_day);
-        else amounts[it] = (days - prev_day);
+    
+    if (money_sums.begin()->second.size() == 1) {
+        auto elem = money_sums.begin()->second.begin();
+        amounts[*elem] += (days - prev_day);                                                                                    
     }
-
+    
     for (auto& it : amounts) {
-        cout << it.first << " " << it.second;
+        cout << it.first << " " << it.second << endl;
     }
 }
+
+// 5
+// Abramovich ABCD 15
+// Deripaska abcd 10
+// Potanin abcd 5
+// Berezovsky ABCD 2
+// Khodorkovsky Chita 1
+// 25 9
+// 1 Abramovich Anadyr
+// 5 Potanin Courchevel
+// 10 Abramovich abcd
+// 11 Abramovich ABCD
+// 11 Deripaska StPetersburg
+// 15 Potanin Norilsk
+// 20 Berezovsky Tbilisi
+// 21 Potanin StPetersburg
+// 22 Berezovsky ABCD
